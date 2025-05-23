4e765a8433b66c72d71b2c0200182e20cbeed6de
Jayesh Prajapati <jayeshprajapati.0904@gmail.com>
2025-05-23 14:18:40 +0530

    Zod schema for room updated

--- packages/zodSchemas/src/userSchema.ts
+++ packages/zodSchemas/src/userSchema.ts
@@ -1,36 +1,36 @@
 import { z } from "zod";
 
 export const SignUpSchema = z.object({
-    username: z
-        .string()
-        .min(3, { message: "Username must be at least 3 characters long" })
-        .max(10, { message: "username should not be greater than 10 Char" }),
-    email: z.string().email(),
-    password: z
-        .string()
-        .min(5, { message: "Password must be at least 5 characters long" })
-        .max(10, { message: "Password must be at most 10 characters long" })
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).*$/, {
-            message:
-                "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.",
-        }),
+	username: z
+		.string()
+		.min(3, { message: "Username must be at least 3 characters long" })
+		.max(10, { message: "username should not be greater than 10 Char" }),
+	email: z.string().email(),
+	password: z
+		.string()
+		.min(5, { message: "Password must be at least 5 characters long" })
+		.max(10, { message: "Password must be at most 10 characters long" })
+		.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).*$/, {
+			message:
+				"Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+		}),
 });
 export const SignInSchema = z.object({
-    username: z
-        .string()
-        .min(3, { message: "Username must me min of 3 Char" })
-        .max(10, { message: "username should not be greater than 10 Char" }),
-    password: z
-        .string()
-        .min(5, { message: "Password must be at least 5 characters long" })
-        .max(10, { message: "Password must be at most 10 characters long" })
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).*$/, {
-            message:
-                "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.",
-        }),
+	username: z
+		.string()
+		.min(3, { message: "Username must me min of 3 Char" })
+		.max(10, { message: "username should not be greater than 10 Char" }),
+	password: z
+		.string()
+		.min(5, { message: "Password must be at least 5 characters long" })
+		.max(10, { message: "Password must be at most 10 characters long" })
+		.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).*$/, {
+			message:
+				"Password must include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+		}),
 });
 
 export const RoomSchema = z.object({
-    roomId: z.string().length(5, { message: "Room id must be 5 char long" }),
-    roomName: z.string().min(8).max(12),
+	roomSlug: z.string().length(5, { message: "Room id must be 5 char long" }),
+	adminId: z.number(),
 });
